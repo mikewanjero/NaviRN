c02d5ed11005932993d9bda9aec5f7fc07b629ac
mikewanjero <mikewanj@gmail.com>
2024-04-18 14:21:14 +0300

    Details Screen page

---
+++ components/DetailsScreen.js
@@ -0,0 +1,22 @@
+import * as React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+export function DetailsScreen({ navigation }) {
+  return (
+    <View style={styles.details}>
+      <Text>Details Screen!</Text>
+      <Button title="Goto Home" onPress={() => navigation.navigate("Home")} />
+      <StatusBar style="auto" backgroundColor="#aaaaaa" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  details: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff6ff",
+  },
+});
