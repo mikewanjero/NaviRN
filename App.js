620146614a2b9f1f6290ef4b6a9de09c0e09d8d7
mikewanjero <mikewanj@gmail.com>
2024-04-26 17:15:59 +0300

    Removed bottom tabs for now

--- App.js
+++ App.js
@@ -1,18 +1,15 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
-import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
-import { HomeScreen } from "./components/HomeScreen";
-import { DetailsScreen } from "./components/DetailsScreen";
-import { NotiScreen } from "./components/NotiScreen";
-import { ProfileScreen } from "./components/ProfileScreen";
-import { SettingsScreen } from "./components/SettingsScreen";
+import { HomeScreen } from "./screens/HomeScreen";
+import { DetailsScreen } from "./screens/DetailsScreen";
+import { NotiScreen } from "./screens/NotiScreen";
+import { ProfileScreen } from "./screens/ProfileScreen";
+import { SettingsScreen } from "./screens/SettingsScreen";
 import { View, Image, Text, StyleSheet } from "react-native";
-import { Table, TableWrapper, Row } from "react-native-table-component";
 import "react-native-gesture-handler";
 
 const Drawer = createDrawerNavigator();
-const Tab = createMaterialBottomTabNavigator();
 
 function LogoTitle({ title }) {
   return (
@@ -47,7 +44,7 @@ function Sidebar() {
       <Drawer.Screen
         name="Home"
         component={HomeScreen}
-        options={({ navigation }) => ({
+        options={() => ({
           headerTitle: (props) => <LogoTitle {...props} title="Overview" />,
           headerStyle: { backgroundColor: "coral" },
           headerTintColor: "#000fed",
@@ -56,7 +53,7 @@ function Sidebar() {
       <Drawer.Screen
         name="Details"
         component={DetailsScreen}
-        options={({ navigation }) => ({
+        options={() => ({
           headerTitle: (props) => <LogoTitle {...props} title="Page Details" />,
           headerStyle: { backgroundColor: "coral" },
           headerTintColor: "#000fed",
@@ -66,7 +63,7 @@ function Sidebar() {
       <Drawer.Screen
         name="Notifications"
         component={NotiScreen}
-        options={({ navigation }) => ({
+        options={() => ({
           headerTitle: (props) => (
             <LogoTitle {...props} title="User Notifications" />
           ),
@@ -77,7 +74,7 @@ function Sidebar() {
       <Drawer.Screen
         name="Profile"
         component={ProfileScreen}
-        options={({ navigation }) => ({
+        options={() => ({
           headerTitle: (props) => <LogoTitle {...props} title="User Profile" />,
           headerStyle: { backgroundColor: "coral" },
           headerTintColor: "#000fed",
@@ -86,62 +83,13 @@ function Sidebar() {
       <Drawer.Screen
         name="Settings"
         component={SettingsScreen}
-        options={({ navigation }) => ({
+        options={() => ({
           headerTitle: (props) => <LogoTitle {...props} title="Settings" />,
           headerStyle: { backgroundColor: "coral" },
           headerTintColor: "#000fed",
         })}
       />
-      <Drawer.Screen
-        name="BottomTabs"
-        component={BottomTabs}
-        options={{
-          headerShown: false,
-        }}
-      />
     </Drawer.Navigator>
-  );
-}
-
-function BottomTabs() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          tabBarIcon: "home",
-        }}
-      />
-      <Tab.Screen
-        name="Details"
-        component={DetailsScreen}
-        options={{
-          tabBarIcon: "information",
-        }}
-      />
-      <Tab.Screen
-        name="Notifications"
-        component={NotiScreen}
-        options={{
-          tabBarIcon: "bell",
-        }}
-      />
-      <Tab.Screen
-        name="Profile"
-        component={ProfileScreen}
-        options={{
-          tabBarIcon: "account",
-        }}
-      />
-      <Tab.Screen
-        name="Settings"
-        component={SettingsScreen}
-        options={{
-          tabBarIcon: "cog",
-        }}
-      />
-    </Tab.Navigator>
   );
 }
 
